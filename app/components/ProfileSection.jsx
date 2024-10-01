54894c4a8355ef51a3141d2cfe548542a762abb7
xpat <patrycja.kruczynska@gmail.com>
2024-10-01 20:20:45 +0200

    buttons and new picture

--- app/components/ProfileSection.jsx
+++ app/components/ProfileSection.jsx
@@ -6,10 +6,10 @@ const ProfileSection = () => {
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+          <h1 className="text-white mb-4 text-5xl lg:text-6xl font-extrabold">
             Hello, I'm Patrycja
           </h1>
-          <p className="text-[#ADB7BE] text-lg lg:text-xl">
+          <p className="text-[#ADB7BE] mb-6 text-lg lg:text-xl">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -17,14 +17,25 @@ const ProfileSection = () => {
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur.
           </p>
+          <div>
+            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+              Embauchez-moi
+            </button>
+            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
+              Téléchargez le CV
+            </button>
+          </div>
         </div>
-        <div className="col-span-5">
-          <Image
-            src="/images/profile-4.png"
-            alt="profile picture"
-            width={300}
-            height={300}
-          />
+        <div className="col-span-5 lg:ml-10 mt-10 lg:mt-4 place-self-center">
+          <div className="rounded-full bg-[#181818] w-[350px] h-[350px] relative">
+            <Image
+              src="/images/robot.png"
+              alt="profile picture"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
         </div>
       </div>
     </section>
