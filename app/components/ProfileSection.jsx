3640d4f6a537f0ddb988521825fd0ffe47c6c5a0
xpat <patrycja.kruczynska@gmail.com>
2024-10-03 13:37:26 +0200

    adding apropos

--- app/components/ProfileSection.jsx
+++ app/components/ProfileSection.jsx
@@ -9,7 +9,7 @@ const ProfileSection = () => {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+            <div className="text-transparent bg-clip-text bg-gradient-to-r from-gray-400 via-blue-700 to-gray-900">
               Hello, I'm{""}
             </div>
             <TypeAnimation
@@ -27,6 +27,10 @@ const ProfileSection = () => {
               speed={50}
               style={{ fontSize: "1.2em", display: "inline-block" }}
               repeat={Infinity}
+              bg-gradient-to-r
+              from-gray-700
+              via-rose-500
+              to-orange-400
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
@@ -34,10 +38,10 @@ const ProfileSection = () => {
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-r from-gray-800 via-blue-700 to-gray-900 hover:bg-slate-200 text-white">
               Embauchez-moi
             </button>
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3">
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-r from-gray-800 via-blue-700 to-gray-900 hover:bg-slate-800 text-white  mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Téléchargez le CV
               </span>
@@ -45,9 +49,9 @@ const ProfileSection = () => {
           </div>
         </div>
         <div className="col-span-5 lg:ml-10 mt-10 lg:mt-4 place-self-center">
-          <div className="rounded-full bg-[#181818] w-[350px] h-[350px] relative">
+          <div className="rounded-full bg-[#181818] w-[330px] h-[330px] relative">
             <Image
-              src="/images/robot.png"
+              src="/images/profile-circle.png"
               alt="profile picture"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
--- app/page.js
+++ app/page.js
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import ProfileSection from "./components/ProfileSection";
 import Navbar from "./components/Navbar";
+import Apropos from "./components/Apropos";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-      <container class="container mt-24 mx-auto px-12 py-4">
+      <div className="container mt-24 mx-auto px-12 py-4">
         <ProfileSection />
-      </container>
+        <Apropos />
+      </div>
     </main>
   );
 }
