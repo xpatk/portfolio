278ea43a3d9345ba415c7d16439388d9f13eea46
xpat <patrycja.kruczynska@gmail.com>
2024-10-03 14:21:06 +0200

    apropos

--- app/components/Apropos.jsx
+++ app/components/Apropos.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Image from "next/image";
 
 const Apropos = () => {
-  return <div>Apropos</div>;
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/comp.png" width={500} height={500} />
+        <div>
+          <h2>A propos de moi</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Apropos;
