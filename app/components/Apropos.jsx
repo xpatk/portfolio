1ad37180da0260c97f0637d797b9cbc9cc6a6ae8
xpat <patrycja.kruczynska@gmail.com>
2024-10-03 14:30:39 +0200

    added hooks for tabnavigation

--- app/components/Apropos.jsx
+++ app/components/Apropos.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+"use client";
+import React, { useTransition, useState } from "react";
 import Image from "next/image";
 
 const Apropos = () => {
+  const [tab, setTab] = useState("skills");
+  const [startTransition, isPending] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
   return (
     <section className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image src="/images/comp.png" width={500} height={500} />
         <div>
-          <h2>A propos de moi</h2>
-          <p>
+          <h2 className="text-4xl gont-bold text-white mb-4">
+            A propos de moi
+          </h2>
+          <p className="text-base lg:text-lg">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
             ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -18,6 +30,13 @@ const Apropos = () => {
             amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
             ut labore et dolore magna aliqua.
           </p>
+          <div className="flex flex-row mt-8">
+            <span className="mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-blue-600">
+              Compétences
+            </span>
+            <span>Éducation</span>
+            <span>Éxperience</span>
+          </div>
         </div>
       </div>
     </section>
