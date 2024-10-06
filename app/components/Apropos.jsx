e91e078472850b8ab4a00b63fded8a07bf6b41ab
xpat <patrycja.kruczynska@gmail.com>
2024-10-06 15:40:21 +0200

    changed image style

--- app/components/Apropos.jsx
+++ app/components/Apropos.jsx
@@ -60,7 +60,7 @@ const Apropos = () => {
   return (
     <section className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/comp.png" width={500} height={500} />
+        <Image src="/images/compdraw.webp" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">
             A propos de moi
