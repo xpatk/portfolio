81195be0b64f5e5f4443fcd704d8abf9d93d66e0
xpat <patrycja.kruczynska@gmail.com>
2024-10-13 19:31:32 +0200

    changes made to title

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 "use client";
-import Head from "next/head";
 import Image from "next/image";
 import ProfileSection from "./components/ProfileSection";
 import Navbar from "./components/Navbar";
@@ -11,24 +10,16 @@ import AchievementSection from "./components/AchievementSection";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Patrycja Kruczyńska - Web Dev</title>
-        <meta name="description" content="Mon portfolio developpeur" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="flex min-h-screen flex-col bg-[#121212]">
-        <Navbar />
-        <div className="container mt-24 mx-auto px-12 py-4">
-          <ProfileSection />
-          <AchievementSection />
-          <Apropos />
-          <ProjectsSection />
-          <EmailSection />
-        </div>
-        <Footer />
-      </main>
-    </>
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <ProfileSection />
+        <AchievementSection />
+        <Apropos />
+        <ProjectsSection />
+        <EmailSection />
+      </div>
+      <Footer />
+    </main>
   );
 }
